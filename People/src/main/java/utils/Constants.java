94f09331d3e13244f3f35c6cfc74b461a52facd7
AdrianPalmadev <adrianmilwid@gmail.com>
2025-04-30 13:00:04 +0200

    Fix #15 Constants

---
+++ People/src/main/java/utils/Constants.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author Adrián Palma
+ */
+public class Constants {
+
+    public static final String STORAGE_ARRAYLIST = "ArrayList";
+    public static final String STORAGE_HASHMAP = "HashMap";
+    public static final String STORAGE_FILE = "File";
+    public static final String STORAGE_FILE_S = "File (Serialization)";
+    public static final String STORAGE_SQL = "SQL";
+    public static final String STORAGE_JPA = "JPA";
+
+}
